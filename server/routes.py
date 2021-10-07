6a54d227f53ae824cd9222a704e7c02049485ad9
Mathew Payne <2772944+GeekMasher@users.noreply.github.com>
2021-10-07 11:14:52 +0000

    Fix security issues

--- server/routes.py
+++ server/routes.py
@@ -13,7 +13,7 @@ def index():
 
     if name:
         cursor.execute(
-            "SELECT * FROM books WHERE name LIKE '%" + name + "%'"
+            "SELECT * FROM books WHERE name LIKE :name", {'name': f"%{name}%"}
         )
         books = [Book(*row) for row in cursor]
         if len(books) == 0:
@@ -21,7 +21,7 @@ def index():
 
     elif author:
         cursor.execute(
-            "SELECT * FROM books WHERE author LIKE '%" + author + "%'"
+            "SELECT * FROM books WHERE author LIKE :author", {'author': f"%{author}%"}
         )
         books = [Book(*row) for row in cursor]
         if len(books) == 0:
