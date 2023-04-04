6e85b8f49a00a1d4c991667818b2cefc102bd724
Eldrick19 <eldrick19@github.com>
2023-04-04 18:36:04 +0000

    Added books class

--- server/models/books.py
+++ server/models/books.py
@@ -2,7 +2,22 @@
 from dataclasses import dataclass
 
 @dataclass
-class Book():
-    name: str
-    author: str
-    read: bool
+class Book:
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+        self.current_page = 1
+
+    def turn_page(self, forward=True):
+        if forward:
+            self.current_page += 1
+        else:
+            self.current_page -= 1
+
+    def __str__(self):
+        return f"{self.title} by {self.author}, {self.pages} pages, currently on page {self.current_page}"
+    
+    def get_author(self):
+        return self.author
+
